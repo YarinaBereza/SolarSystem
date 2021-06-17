1ac049bf772b4fa99c4564c9b818accdbdd3026f
YarinaBereza <yarinabereza@gmail.com>
2021-06-17 17:45:29 +0300

    the features of planet 2 are done and added successfully

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,16 @@ public class Main {
         FactorX.Sun=1;
         FactorX.Moon=3;
         FactorX.Planets=2;
+
+        //working on sun features
+        Sun Feron = new Sun();
+        Feron.Heat="600000000000kj";
+        Feron.Radius="823587325872385723895km/2";
+        Feron.Temperature="100000000000000000000000000f";
+
+        Planet2 Picaro = new Planet2();
+        Picaro.radius="8235873258723857";
+        Picaro.size = "big";
+
     }
 }
